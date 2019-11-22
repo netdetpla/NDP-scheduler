8a5e916d7b9d9cd9862c36163ddc349474734fbf
SinLapis <stradust0001@gmail.com>
2019-11-22 17:26:18 +0800

    add executor selection

--- src/DatabaseHandler.go
+++ src/DatabaseHandler.go
@@ -24,6 +24,7 @@ type taskInfo struct {
 	tag       string
 	param     string
 	priority  int
+	executorIP string
 }
 
 var scanOpt = make(chan dbOpt, 50)
@@ -84,7 +85,18 @@ func scanTaskStatus(db *sql.DB) (err error) {
 	scanOpt <- dbOpt{"task", []string{}}
 	return
 }
-func scanTask(db *sql.DB) (err error) {
+func scanTask(db *sql.DB, executorIP string) (err error) {
+	unlockFlag := true
+	// 退出前如果没有任务或者出错则解锁
+	defer func() {
+		if unlockFlag {
+			unlockSQL := "update executor set status = 0 where exec_ip = ?"
+			_, err = db.Exec(unlockSQL, executorIP)
+			if err != nil {
+				log.Warning(err.Error())
+			}
+		}
+	}()
 	minPrioritySQL := "select MIN(priority) from task where task_status = 20000"
 	var minPriority sql.NullInt64
 	err = db.QueryRow(minPrioritySQL).Scan(&minPriority)
@@ -110,8 +122,10 @@ func scanTask(db *sql.DB) (err error) {
 		log.Warning(err.Error())
 		return
 	}
+	i.executorIP = executorIP
 	if checkImage(db, i.id) {
 		taskQueue <- *i
+		unlockFlag = false
 	}
 	return
 }
@@ -150,6 +164,29 @@ func insertResult(db *sql.DB, resultLine string, taskID string, table string) (e
 	return
 }
 
+// 扫描执行节点状态
+func scanExecutor(db *sql.DB) (err error){
+	checkSQL := "select exec_ip from executor where status = 0 limit 1"
+	var result sql.NullString
+	err = db.QueryRow(checkSQL).Scan(&result)
+	if err != nil {
+		log.Warning(err.Error())
+		return
+	}
+	if !result.Valid {
+		return
+	}
+	// 锁定对应节点
+	lockSQL := "update executor set status = 1 where exec_ip = ?"
+	_, err = db.Exec(lockSQL, result.String)
+	if err != nil {
+		log.Warning(err.Error())
+		return
+	}
+	scanOpt <- dbOpt{"task", []string{result.String}}
+	return
+}
+
 // 任务扫描定时器
 func taskTimer() {
 	for true {
@@ -165,6 +202,16 @@ func imageTimer() {
 		scanOpt <- dbOpt{"image", []string{}}
 	}
 }
+
+// 执行节点扫描定时器
+func executorTimer() {
+	for true {
+		time.Sleep(20 * time.Second)
+		scanOpt <- dbOpt{"executor", []string{}}
+	}
+}
+
+
 
 // 根据定时器信号启动对应的数据库查询操作
 func databaseScanner(databaseInfo *database) {
@@ -194,20 +241,23 @@ func databaseScanner(databaseInfo *database) {
 		return
 	}
 	// 启动定时器
-	go taskTimer()
+	//go taskTimer()
 	go imageTimer()
+	go executorTimer()
 	scanOpt <- dbOpt{"image", []string{}}
-	scanOpt <- dbOpt{"status", []string{}}
+	scanOpt <- dbOpt{"executor", []string{}}
 	for {
 		// 读取channel里消息并调用对应方法，没有则阻塞等待
 		so := <-scanOpt
 		switch so.operation {
 		case "image":
 			err = scanImage(mysqlDB)
-		case "status":
-			err = scanTaskStatus(mysqlDB)
+		//case "status":
+		//	err = scanTaskStatus(mysqlDB)
 		case "task":
-			err = scanTask(mysqlDB)
+			err = scanTask(mysqlDB, so.param[0])
+		case "executor":
+			err = scanExecutor(mysqlDB)
 		case "loaded":
 			err = updateImageLoadedStatus(mysqlDB, so.param[0], so.param[1])
 		case "task-status":
